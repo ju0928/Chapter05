a61ba41941035700c50b331d2b39a9dec03cca68
주연 장 <gg2222222@naver.com>
2023-03-10 15:30:53 +0900

    202303101530: p149 예제

--- IfElse/MainApp.cs
+++ IfElse/MainApp.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IfElse
 {
@@ -10,7 +6,31 @@ namespace IfElse
     {
         static void Main(string[] args)
         {
-            // 첫번째 커밋 진행.
+            Console.WriteLine("숫자를 입력하세요. : ");
+
+            string input = Console.ReadLine();
+            int number = Int32.Parse(input);
+
+            if (number < 0)
+            {
+                Console.WriteLine("음수");
+            }
+            else if (number >0)
+            {
+                Console.WriteLine("양수");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+            if (number % 2==0)
+            {
+                Console.WriteLine("짝수");
+            }
+            else
+            {
+                Console.WriteLine("홀수");
+            }
         }
     }
 }
