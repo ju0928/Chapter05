4d4b6404028bd57c615834bc37b6dfc4eb7977be
주연 장 <gg2222222@naver.com>
2023-03-13 15:38:43 +0900

    202303131538

---
+++ P151_ex2/MainApp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace P151_ex2
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("점수를 입력하세요. : ");
+
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+
+            if (number >= 90)
+            {
+                Console.WriteLine($"입력하신 점수{number}는(은) A 학점 입니다.");
+            }
+            else if (number >= 80)
+            {
+                Console.WriteLine($"입력하신 점수{number}는(은) B 학점 입니다.");
+            }
+            else if (number >= 70)
+            {
+                Console.WriteLine($"입력하신 점수{number}는(은) C 학점 입니다.");
+            }
+            else
+            {
+                Console.WriteLine($"입력하신 점수{number}는(은) F 학점 입니다.");
+            }
+        }
+    }
+}
---
+++ P151_ex3/MainApp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace P151_ex3
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("숫자를 입력하세요. : ");
+
+            string input = Console.ReadLine();
+            int number = Int32.Parse(input);
+
+
+            if (number >= 50)
+            {
+                Console.WriteLine($"입력하신 숫자{number}는(은) 50이상의 수 입니다.");
+            }
+            else
+            {
+                Console.WriteLine($"입력하신 숫자{number}는(은) 50미만의 수 입니다.");
+            }
+
+        }
+    }
+}
