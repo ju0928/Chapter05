81b28aea063ee84f28b0061bf2d8cbef0407c9db
ju0928 <gg2222222@naver.com>
2023-03-13 20:47:04 +0900

    202303132046

---
+++ P151_ex4/MainApp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace P151_ex4
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("정수 한 개를 입력하세요");
+
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+
+            if (number % 3 == 0)
+            {
+                Console.WriteLine($"입력하신 정수{number}는(은) 3의 배수입니다.");
+            }
+            else
+            {
+                Console.WriteLine($"입력하신 정수{number}는(은) 3의 배수가 아닙니다.");
+            }
+        }
+    }
+}
