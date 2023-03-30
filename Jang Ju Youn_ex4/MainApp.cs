c9ed5acce660d0f4fc6a1c2591df0982c8e32940
주연 장 <gg2222222@naver.com>
2023-03-30 13:06:58 +0900

    202303301306

--- Jang Ju Youn_ex4/MainApp.cs
+++ Jang Ju Youn_ex4/MainApp.cs
@@ -6,6 +6,26 @@ namespace Jang_Ju_Youn_ex4
     {
         static void Main(string[] args)
         {
+            Console.Write("반복 횟수를 입력하세요 : ");
+            string input = Console.ReadLine();
+
+            int number = Convert.ToInt32(input); 
+
+            if (number <= 0)
+            {
+                Console.WriteLine("0보다 작거나 같은 수는 입력할 수 없습니다.");
+            }
+            else
+            {
+                for (int i = 0; i < number; i++)
+                {
+                    for (int j = 0; j < i + 1; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
